d88ecda46d44beaaa22f8bcf2f2a3ec9456c7f2f
FarisDnial <faris.danial.fauzi@gmail.com>
2024-07-31 21:37:04 +0800

    3- deleted post added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,7 +27,6 @@ function App() {
           throw new Error(data.error || "Something went wrong");
         }
 
-        console.log("authUser is here:", data);
         return data;
 
       } catch (error) {
